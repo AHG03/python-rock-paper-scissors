91d4a6739854643033de05c17fb805c3970f61ea
Ali Ahangar <aliahangar80@gmail.com>
2025-10-22 19:07:08 +0330

    refactor code and create display_choices, determine_winner, play_game functions.

--- rps.py
+++ rps.py
@@ -11,24 +11,33 @@ def get_user_choice():
         else:
             print("Invalid choice")
 
-while True:
-    user_choice = get_user_choice()
-
-    computer_choice = random.choice(choices)
-
+def display_choices(user_choice, computer_choice):
     print(f"you chose {emojis[user_choice]}")
     print(f"computer chose {emojis[computer_choice]}")
 
+def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Draw")
     elif (
-        (user_choice == "r" and computer_choice == "s") or
-        (user_choice == "s" and computer_choice == "p") or
-        (user_choice == "p" and computer_choice == "r")):
+            (user_choice == "r" and computer_choice == "s") or
+            (user_choice == "s" and computer_choice == "p") or
+            (user_choice == "p" and computer_choice == "r")):
         print("you win")
     else:
         print("you lose")
 
-    should_continue = input("Continue? (y/n): ").lower()
-    if should_continue == "n":
-        break
+def play_game():
+    while True:
+        user_choice = get_user_choice()
+
+        computer_choice = random.choice(choices)
+
+        display_choices(user_choice, computer_choice)
+
+        determine_winner(user_choice, computer_choice)
+
+        should_continue = input("Continue? (y/n): ").lower()
+        if should_continue == "n":
+            break
+
+play_game()
