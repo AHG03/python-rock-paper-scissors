0a7eb545dfdbc043ee025273657f891081eaab85
Ali Ahangar <aliahangar80@gmail.com>
2025-10-22 19:33:48 +0330

    create constants and convert choices list to tuple.

--- rps.py
+++ rps.py
@@ -1,7 +1,10 @@
 import random
 
-choices = ["r", "p", "s"]
-emojis = {"r":"💎", "p":"📃", "s":"✂"}
+ROCK = "r"
+PAPER = "p"
+SCISSORS = "s"
+emojis = {ROCK:"💎", PAPER:"📃", SCISSORS:"✂"}
+choices = tuple(emojis.keys())
 
 def get_user_choice():
     while True:
@@ -19,9 +22,9 @@ def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Draw")
     elif (
-            (user_choice == "r" and computer_choice == "s") or
-            (user_choice == "s" and computer_choice == "p") or
-            (user_choice == "p" and computer_choice == "r")):
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == SCISSORS and computer_choice == PAPER) or
+        (user_choice == PAPER and computer_choice == ROCK)):
         print("you win")
     else:
         print("you lose")
